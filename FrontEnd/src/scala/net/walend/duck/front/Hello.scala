46537b236844b6ac1bb7dd221e381a3bfdbe8a3b
dwalend <david@walend.net>
2024-09-09 23:15:28 -0400

    One geolocation call

--- FrontEnd/src/scala/net/walend/duck/front/Hello.scala
+++ FrontEnd/src/scala/net/walend/duck/front/Hello.scala
@@ -1,13 +1,26 @@
 package net.walend.duck.front
 
-import org.scalajs.dom.document
+import org.scalajs.dom.{Geolocation, Position, PositionError, document}
 
-object Hello {
-  def main(args: Array[String]): Unit = {
+object Hello:
+  def main(args: Array[String]): Unit =
     println("Hello ducks!")
     val parNode = document.createElement("p")
     val textNode = document.createTextNode("Hello, world")
     parNode.appendChild(textNode)
     document.body.appendChild(parNode)
-  }
-}
+    geolocate()
+
+
+  def geolocate(): Unit =
+    val window = document.defaultView
+    val nav = window.navigator
+    val geo: Geolocation = nav.geolocation
+
+    def onSuccess(p: Position): Unit =
+      println(s"latitude=${p.coords.latitude}") // Latitude
+      println(s"longitude=${p.coords.longitude}") // Longitude
+
+    def onError(p: PositionError): Unit = println(s"Error $p")
+
+    geo.getCurrentPosition(onSuccess _,onError _)
