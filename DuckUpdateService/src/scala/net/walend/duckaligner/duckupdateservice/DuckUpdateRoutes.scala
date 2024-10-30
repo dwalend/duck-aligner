ae12969084ffe6867b92786f5a9ff96fe469db03
dwalend <david@walend.net>
2024-10-30 15:13:19 -0400

    Got some advice from Cats Effects discord - Async is the right answer

--- DuckUpdateService/src/scala/net/walend/duckaligner/duckupdateservice/DuckUpdateRoutes.scala
+++ DuckUpdateService/src/scala/net/walend/duckaligner/duckupdateservice/DuckUpdateRoutes.scala
@@ -6,7 +6,7 @@ package net.walend.duckaligner.duckupdateservice
  * @author David Walend
  * @since v0.0.0
  */
-import net.walend.duckaligner.duckupdates.v0.DuckUpdateServiceGen
+import net.walend.duckaligner.duckupdates.v0.{DuckUpdateService, DuckUpdateServiceGen}
 import cats.effect.*
 import cats.implicits.toFunctorOps
 import org.http4s.implicits.*
@@ -14,7 +14,6 @@ import org.http4s.ember.server.*
 import org.http4s.*
 import com.comcast.ip4s.*
 import net.walend.duckaligner.duckupdateservice.store.DucksStateStore
-import smithy4s.http4s
 import smithy4s.http4s.SimpleRestJsonBuilder
 
 object Routes {
@@ -23,13 +22,12 @@ object Routes {
     SimpleRestJsonBuilder.routes(ducksStateStore).resource
   }
 
-/*
-  private val duckStoreRoutes: Resource[IO, HttpRoutes[IO]] = SimpleRestJsonBuilder.routes(DucksStateStore.makeDuckStateStore[IO],IO).resource
-*/
-//  private val docs = smithy4s.http4s.swagger.docs[IO](DucksStateStore)
+  private def docs[F[_]: Sync] =
+    smithy4s.http4s.swagger.docs[F](DuckUpdateService)
 
-  def allF[F[_]: Concurrent]: F[Resource[F, HttpRoutes[F]]] = duckStoreRoutesF[F]
-
+  def allF[F[_] : Async ]: F[Resource[F, HttpRoutes[F]]] =
+    import cats. syntax. all. toSemigroupKOps
+    duckStoreRoutesF[F].map(_.map(_ <+> docs))
 }
 
 object Main extends IOApp.Simple:
