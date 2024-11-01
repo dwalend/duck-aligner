3f41f457e6ab52903c134f9c8192b5e6c18f8b4c
dwalend <david@walend.net>
2024-11-01 18:36:00 -0400

    Now serving static files from the duck update service

--- DuckUpdateService/src/scala/net/walend/duckaligner/duckupdateservice/DuckUpdateRoutes.scala
+++ DuckUpdateService/src/scala/net/walend/duckaligner/duckupdateservice/DuckUpdateRoutes.scala
@@ -9,37 +9,20 @@ package net.walend.duckaligner.duckupdateservice
 import net.walend.duckaligner.duckupdates.v0.{DuckUpdateService, DuckUpdateServiceGen}
 import cats.effect.*
 import cats.implicits.toFunctorOps
-import org.http4s.implicits.*
-import org.http4s.ember.server.*
 import org.http4s.*
-import com.comcast.ip4s.*
 import net.walend.duckaligner.duckupdateservice.store.DucksStateStore
 import smithy4s.http4s.SimpleRestJsonBuilder
 
-object Routes {
+object DuckUpdateRoutes {
 
   private def duckStoreRoutesF[F[_]: Concurrent]: F[Resource[F, HttpRoutes[F]]] = DucksStateStore.makeDuckStateStore[F].map { ducksStateStore =>
     SimpleRestJsonBuilder.routes(ducksStateStore).resource
   }
 
-  private def docs[F[_]: Sync] =
-    smithy4s.http4s.swagger.docs[F](DuckUpdateService)
+  private def docs[F[_]: Sync]: HttpRoutes[F] =
+    smithy4s.http4s.swagger.docs[F](DuckUpdateService)  //todo remove this before running it for real
 
   def allF[F[_] : Async ]: F[Resource[F, HttpRoutes[F]]] =
     import cats. syntax. all. toSemigroupKOps
-    duckStoreRoutesF[F].map(_.map(_ <+> docs))
+    duckStoreRoutesF[F].map(_.map(_.combineK(docs)))
 }
-
-object Main extends IOApp.Simple:
-
-  val run: IO[Nothing] = Routes.allF[IO]
-    .flatMap { routes =>
-      routes.flatMap { r =>
-        EmberServerBuilder
-          .default[IO]
-          .withPort(port"9000")
-          .withHost(host"localhost")
-          .withHttpApp(r.orNotFound)
-          .build
-      }.use(_ => IO.never)
-    }
---
+++ DuckUpdateService/src/scala/net/walend/duckaligner/duckupdateservice/StaticRoutes.scala
@@ -0,0 +1,28 @@
+package net.walend.duckaligner.duckupdateservice
+
+import cats.MonadThrow
+import fs2.io.file.Files
+import org.http4s.{HttpRoutes, StaticFile}
+import org.http4s.dsl.Http4sDsl
+
+/**
+ * Serve static files
+ *
+ * @author David Walend
+ * @since v0.0.0
+ */
+object StaticRoutes {
+  def staticFiles[F[_]: MonadThrow : Files]:HttpRoutes[F] =
+    import fs2.io.file.Path
+    val dsl = new Http4sDsl[F]{}
+    import dsl.*
+    HttpRoutes.of[F] {
+      //todo so wrong. Needs a rework via bleep
+      case request@GET -> Root / "static" / "hello.html" =>
+        StaticFile.fromPath(Path("/Users/dwalend/projects/duck-aligner/FrontEnd/hello.html"), Some(request))
+          .getOrElseF(NotFound()) // In case the file doesn't exist
+      case request@GET -> Root / "static" / "FrontEnd.js" =>
+        StaticFile.fromPath(Path("/Users/dwalend/projects/duck-aligner/.bleep/builds/normal/.bloop/FrontEnd/FrontEnd.js"), Option(request))
+          .getOrElseF(NotFound()) // In case the file doesn't exist
+    }
+}
