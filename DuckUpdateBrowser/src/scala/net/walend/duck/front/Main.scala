90fbad6a10da8f8afd0f21c669baba2cfce80aec
dwalend <david@walend.net>
2025-02-17 18:08:21 -0500

    Closing the loop on start

--- DuckUpdateBrowser/src/scala/net/walend/duck/front/Main.scala
+++ DuckUpdateBrowser/src/scala/net/walend/duck/front/Main.scala
@@ -4,7 +4,7 @@ import calico.IOWebApp
 import cats.effect.{IO, Resource}
 import org.scalajs.dom.{Position, document}
 import fs2.dom.{HtmlDocument, HtmlElement}
-import net.walend.duckaligner.duckupdates.v0.MapLibreGlKeyOutput
+import net.walend.duckaligner.duckupdates.v0.{DuckId, DuckUpdate, DuckUpdateService, GeoPoint, UpdatePositionOutput}
 
 import scala.annotation.unused
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
@@ -23,43 +23,46 @@ object Main extends IOWebApp:
     import calico.html.io.{*, given}
 
     for
-      client <- DuckUpdateClient.duckUpdateClient
+      client: DuckUpdateService[IO] <- DuckUpdateClient.duckUpdateClient
       apiKey: String <- client.mapLibreGlKey().map(_.key).toResource
 
       doc: HtmlDocument[IO] = window.document
       //      geoLocator: GeoLocator = GeoLocator.geolocator(document,client)
       geoIO = GeoIO(document)
       position: Position <- geoIO.positionResource()
-
+      update: UpdatePositionOutput <- updatePosition(position,client)
       _ <- IO.println(s"Hello ${position.coords.latitude},${position.coords.longitude}!").toResource
-      hi <- label(s"Hello ${position.coords.latitude},${position.coords.longitude}!")
-      mapDiv <- div("map")
-      mapLibre <- mapLibreResoruce(apiKey,position)
+      appDiv <- div("I wish this were the map")
+      mapLibre <- mapLibreResource(apiKey,update)
     yield
-       /*
-      //todo make this a resource
-      val styleUrl = s"https://maps.geo.$awsRegion.amazonaws.com/v2/styles/$mapStyle/descriptor?key=$apiKey"
-      val map = new Map(new MapOptions {
-        style = styleUrl
-        var container = "map"
-        center = (-71.20792615771647, 42.33588581370238)
-        zoom = 14
-      })  */
-
       println("DuckUpdateClient ducks!")
-//      hi
-      mapDiv
+      appDiv
 
-
-  def mapLibreResoruce(apiKey:String,position: Position): Resource[IO, Map] =
+  private def updatePosition(position: Position,client: DuckUpdateService[IO]): Resource[IO, UpdatePositionOutput] =
+    val geoPoint: GeoPoint = GeoPoint(
+      latitude = position.coords.latitude,
+      longitude = position.coords.longitude,
+      timestamp = position.timestamp.toLong
+    )
+    val duckUpdate: DuckUpdate = DuckUpdate(
+      id = DuckId(0),
+      snapshot = 0,
+      position = geoPoint
+    )
+    client.updatePosition(duckUpdate).toResource  
+  
+  
+  private def mapLibreResource(apiKey:String, update: UpdatePositionOutput): Resource[IO, Map] =
     val mapStyle = "Standard"; // e.g., Standard, Monochrome, Hybrid, Satellite
     val awsRegion = "us-east-1"; // e.g., us-east-2, us-east-1, us-west-2, etc.
 
+    val p: GeoPoint = update.tracks.tracks.head.positions.head
+    
     val styleUrl = s"https://maps.geo.$awsRegion.amazonaws.com/v2/styles/$mapStyle/descriptor?key=$apiKey"
     IO{new Map(new MapOptions {
       style = styleUrl
       var container = "map"
-      center = (position.coords.longitude,position.coords.latitude)
+      center = (p.longitude,p.latitude)
       zoom = 14
     })}.toResource
 
