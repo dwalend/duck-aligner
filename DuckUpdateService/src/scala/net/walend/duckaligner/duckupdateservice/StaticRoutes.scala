1142e9d4418984acfdfa15468e04f25d3e298de2
dwalend <david@walend.net>
2025-03-09 23:32:14 -0400

    Set up to package all the dependencies

--- DuckUpdateService/src/scala/net/walend/duckaligner/duckupdateservice/StaticRoutes.scala
+++ DuckUpdateService/src/scala/net/walend/duckaligner/duckupdateservice/StaticRoutes.scala
@@ -1,6 +1,7 @@
 package net.walend.duckaligner.duckupdateservice
 
 import cats.MonadThrow
+import cats.effect.Sync
 import fs2.io.file.Files
 import org.http4s.{HttpRoutes, StaticFile}
 import org.http4s.dsl.Http4sDsl
@@ -12,27 +13,19 @@ import org.http4s.dsl.Http4sDsl
  * @since v0.0.0
  */
 object StaticRoutes {
-  def staticFiles[F[_]: MonadThrow : Files]:HttpRoutes[F] =
-    import fs2.io.file.Path
+  def staticFiles[F[_]: MonadThrow : Files : Sync]:HttpRoutes[F] =
     val dsl = new Http4sDsl[F]{}
     import dsl.*
     HttpRoutes.of[F] {
-      //todo so wrong. Needs a rework via bleep. Where should static files be served? jar resources? Same for scala-js
       case request@GET -> Root / "static" / "hello.html" =>
-        StaticFile.fromPath(Path("/Users/dwalend/projects/duck-aligner/DuckUpdateBrowser/hello.html"), Option(request))
+        StaticFile.fromResource("hello.html", Option(request))
           .getOrElseF(NotFound()) // In case the file doesn't exist
       case request@GET -> Root / "static" / "main.js" =>
         //./.bleep/builds/normal/.bloop/DuckUpdateBrowser/
-        StaticFile.fromPath(Path("/Users/dwalend/projects/duck-aligner/.bleep/builds/normal/.bloop/DuckUpdateBrowser/DuckUpdateBrowser-js/main.js"), Option(request))
-          .getOrElseF(NotFound()) // In case the file doesn't exist
-      case request@GET -> Root / "static" / "DuckUpdateBrowser.js" =>
-        StaticFile.fromPath(Path("/Users/dwalend/projects/duck-aligner/.bleep/builds/normal/.bloop/DuckUpdateBrowser/DuckUpdateBrowser.js"), Option(request))
-          .getOrElseF(NotFound()) // In case the file doesn't exist
-      case request@GET -> Root / "static" / "map.html" =>
-        StaticFile.fromPath(Path("/Users/dwalend/projects/duck-aligner/DuckUpdateBrowser/map.html"), Option(request))
+        StaticFile.fromResource("main.js", Option(request))
           .getOrElseF(NotFound()) // In case the file doesn't exist
       case request@GET -> Root / "static" / "style.css" =>
-        StaticFile.fromPath(Path("/Users/dwalend/projects/duck-aligner/DuckUpdateBrowser/style.css"), Option(request))
+        StaticFile.fromResource("style.css", Option(request))
           .getOrElseF(NotFound()) // In case the file doesn't exist
     }
 }
