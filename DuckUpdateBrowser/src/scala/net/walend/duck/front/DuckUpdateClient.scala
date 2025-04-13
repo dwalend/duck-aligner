41cc34cb8faf4325fac528dab67fb18ef9bf9b3e
dwalend <david@walend.net>
2025-04-13 14:41:28 -0400

    Got the system working again with a hard-coded stage name

--- DuckUpdateBrowser/src/scala/net/walend/duck/front/DuckUpdateClient.scala
+++ DuckUpdateBrowser/src/scala/net/walend/duck/front/DuckUpdateClient.scala
@@ -12,9 +12,10 @@ import cats.effect.syntax.all.effectResourceOps
 
 
 object DuckUpdateClient:
+  val stageName = "/dev"
   def duckUpdateClient[F[_]: Async]: Resource[F, DuckUpdateService[F]] = for {
     httpClient: Client[F] <- FetchClientBuilder[F].resource
-    origin: Uri <- Window[F].location.origin.flatMap(Uri.fromString(_).liftTo[F]).toResource
+    origin: Uri <- Window[F].location.origin.flatMap(o => Uri.fromString(o+stageName).liftTo[F]).toResource
     duckUpdateClient <- SimpleRestJsonBuilder(DuckUpdateService).client(httpClient).uri(origin).resource
   } yield duckUpdateClient
 
