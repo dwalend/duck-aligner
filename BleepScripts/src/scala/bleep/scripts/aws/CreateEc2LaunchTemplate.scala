ee7b46c63704cb10c3e1ca9ed3ca233f156de596
dwalend <david@walend.net>
2025-04-19 16:14:36 -0400

    Start and stop machine are working

---
+++ BleepScripts/src/scala/bleep/scripts/aws/CreateEc2LaunchTemplate.scala
@@ -0,0 +1,59 @@
+package bleep.scripts.aws
+
+import bleep.{BleepScript, Commands, Started}
+import software.amazon.awssdk.services.ec2.model.{AuthorizeSecurityGroupIngressRequest, CreateLaunchTemplateRequest, CreateSecurityGroupRequest, LaunchTemplateBlockDeviceMappingRequest, LaunchTemplateEbsBlockDeviceRequest, RequestLaunchTemplateData, VolumeType}
+
+object CreateEc2LaunchTemplate extends BleepScript("CreateEc2LaunchTemplate") :
+  override def run(started: Started, commands: Commands, args: List[String]): Unit =
+
+    val ec2Client = CommonAws.ec2Client
+
+    val securityGroupName = "Duck Update Server Security"
+    
+    val createSecurityGroupRequest = CreateSecurityGroupRequest.builder()
+      .groupName(securityGroupName)
+      .description("ssh and port 8080 for the Duck Update service")
+      .build()
+
+    val createSecurityGroupResponse = ec2Client.createSecurityGroup(createSecurityGroupRequest)
+    println(createSecurityGroupResponse)
+
+    val authorizeIngressRequest = AuthorizeSecurityGroupIngressRequest.builder()
+      .groupName(securityGroupName)
+      .ipProtocol("tcp")
+      .toPort(22)
+      .fromPort(22)
+      .cidrIp("74.104.110.185/32")
+      .build
+    val authorizeIngressResponse = ec2Client.authorizeSecurityGroupIngress(authorizeIngressRequest)
+    println(authorizeIngressResponse)
+
+    /// All this to make the launch template
+    val ebsInstanceBlockDeviceSpecification = LaunchTemplateEbsBlockDeviceRequest.builder()
+      .deleteOnTermination(true)
+      .volumeSize(8)
+      .volumeType(VolumeType.GP3)
+      //      .snapshotId("snap-041f7252be0b80435") //pulled this out of the UI. No idea where else to find it
+      .build()
+
+    val blockDeviceMapping = LaunchTemplateBlockDeviceMappingRequest.builder()
+      .ebs(ebsInstanceBlockDeviceSpecification)
+      .deviceName("/dev/xvda")
+      .build()
+
+    val requestLaunchTemplateData = RequestLaunchTemplateData.builder()
+      .keyName("davidAtWalendDotNet")
+      .imageId("aws-0eae2a0fc13b15fce")  //todo need some way to say "latest amazon linux"
+      .instanceType("t4g.nano")
+      .securityGroups(securityGroupName)
+      .ebsOptimized(true)
+      .blockDeviceMappings(blockDeviceMapping)
+      .build()
+
+    val createLaunchTemplateRequest = CreateLaunchTemplateRequest.builder()
+      .launchTemplateName(CommonAws.launchTemplateName)
+      .launchTemplateData(requestLaunchTemplateData)
+      .build()
+
+    val createLaunchTemplateResponse = ec2Client.createLaunchTemplate(createLaunchTemplateRequest)
+    println(createLaunchTemplateResponse)
--- BleepScripts/src/scala/bleep/scripts/aws/PushServiceToEc2.scala
+++ BleepScripts/src/scala/bleep/scripts/aws/PushServiceToEc2.scala
@@ -10,19 +10,22 @@ import software.amazon.awssdk.services.ec2.model.TerminateInstancesRequest
 import java.nio.file.Path
 import scala.jdk.CollectionConverters.{ListHasAsScala, SeqHasAsJava}
 
-object PushServiceToEc2 extends BleepScript("CreateEc2Ami") :
+object PushServiceToEc2 extends BleepScript("PushServiceToEc2") :
   override def run(started: Started, commands: Commands, args: List[String]): Unit =
 
     commands.script(ScriptName("fat-jar"), args)
 
     //get the server's IP address
-    val serverIpAddress: Seq[String] = CommonEc2.describeTestInstances().flatMap(_.networkInterfaces().asScala).map(_.association().publicIp())
+    val serverIpAddress: Seq[String] = CommonAws.describeTestInstances().flatMap(_.networkInterfaces().asScala).map(_.association().publicIp())
+
+    println(s"serverIpAddresses are $serverIpAddress")
 
     val projectName = ProjectName(args.head)
     val localFatJar: Path = FatJar.jarPath(started,projectName)
     //scp the latest fat jar
     serverIpAddress.foreach { hostname =>
       Scp.scpFile(hostname,localFatJar,localFatJar.getFileName.toString)
+      println(s"scp'ed $localFatJar to $hostname")
     }
 
     //ssh command to stop any old java process
