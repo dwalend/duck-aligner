782ad16fda0254abd61b0cc103b2b24c58092651
dwalend <david@walend.net>
2026-02-10 23:37:30 -0500

    Some bettering of ScalablyTypedGenerator

--- BleepScripts/src/scala/bleep/scripts/scalablytyped/ScalablyTypedGenerator.scala
+++ BleepScripts/src/scala/bleep/scripts/scalablytyped/ScalablyTypedGenerator.scala
@@ -2,28 +2,42 @@ package bleep.scripts.scalablytyped
 
 import org.scalablytyped.converter.cli.Main
 import bleep.model.{CrossProjectName, ProjectName}
-import bleep.{BleepCodegenScript, BleepScript, Commands, Started, model}
+import bleep.{BleepCodegenScript, Commands, Started, model}
 import bloop.config.Config
 
 import java.io.File
-import java.nio.file.{Files, Path, Paths, StandardCopyOption}
+import java.nio.file.{Files, Paths, StandardCopyOption}
+import scala.annotation.tailrec
 
 object ScalablyTypedGenerator extends BleepCodegenScript("ScalablyTypedGenerator") :
 
   override def run(started: Started, commands: Commands, targets: List[ScalablyTypedGenerator.Target], args: List[String]): Unit =
     targets.foreach { target =>
       val projectName = target.project.name
+      val packageJsonPath = Paths.get(s"${projectName.value}/package.json")
 
-      val sctMain: Int = Main.mainNoExit(Array(
-        "--directory", projectName.value,
-        "--scala", "3.3.7", //todo from bleep
-        "--scalajs", "1.20.2", //todo my own package and other command line options
-      ))
       val sourcePath = Paths.get(s"${projectName.value}/out")
       val destinationPath = target.sources
-      delete(destinationPath.toFile)
-      Files.createDirectories(destinationPath)
-      Files.move(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING)
+
+      def regenerate:Boolean =
+        !destinationPath.toFile.exists() ||
+        packageJsonPath.toFile.lastModified() > destinationPath.toFile.lastModified()
+
+      if(regenerate)
+        val crossProjectName: CrossProjectName = model.CrossProjectName(projectName, None)
+        val bloopProject: Config.Project = started.bloopFiles(crossProjectName).forceGet.project
+        val scalaVersion =  bloopProject.scala.get.version
+
+        val sctMain: Int = Main.mainNoExit(Array(
+          "--directory", projectName.value,
+          "--scala", scalaVersion,
+          "--scalajs", "1.20.2", //todo from bleep template
+          //todo my own package and other command line options
+        ))
+        delete(destinationPath.toFile)
+        Files.createDirectories(destinationPath)
+        Files.move(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING)
+        ()
     }
 
   def delete(file: File): Unit =
