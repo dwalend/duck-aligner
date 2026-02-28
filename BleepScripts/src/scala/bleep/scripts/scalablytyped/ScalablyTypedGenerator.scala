12c6904c0aa68688cef41ed9999d6cb72490f2cb
dwalend <david@walend.net>
2026-02-28 12:46:04 -0500

    Cleaned up imports to prevent warnings.

--- BleepScripts/src/scala/bleep/scripts/scalablytyped/ScalablyTypedGenerator.scala
+++ BleepScripts/src/scala/bleep/scripts/scalablytyped/ScalablyTypedGenerator.scala
@@ -1,29 +1,12 @@
 package bleep.scripts.scalablytyped
 
-import org.scalablytyped.converter.cli.Main
 import bleep.model.CrossProjectName
 import bleep.{BleepCodegenScript, Commands, Started, model}
 import bloop.config.Config as BloopConfig
-import com.olvind.logging.LogLevel
-import fansi.Color
-import org.scalablytyped.converter.cli.Main.{Config, DefaultConfig, ParseConversionOptions, logger, parseCachePath, table}
-import org.scalablytyped.converter.internal.importer.build.{PublishedSbtProject, SbtProject, ScalaCliCompiler}
-import org.scalablytyped.converter.internal.importer.documentation.Npmjs
-import org.scalablytyped.converter.internal.importer.{Bootstrap, LibScalaJs, LibTsSource, PersistingParser, Phase1ReadTypescript, Phase2ToScalaJs, Phase3Compile, PhaseFlavour}
-import org.scalablytyped.converter.internal.phases.PhaseListener.NoListener
-import org.scalablytyped.converter.internal.phases.{PhaseRes, PhaseRunner, RecPhase}
-import org.scalablytyped.converter.internal.scalajs.Name
-import org.scalablytyped.converter.internal.ts.CalculateLibraryVersion.PackageJsonOnly
-import org.scalablytyped.converter.internal.ts.{PackageJson, TsIdentLibrary}
-import org.scalablytyped.converter.internal.{InFolder, Json, constants, files, sets}
-import scopt.OParser
-import sourcecode.Text.generate
+import org.scalablytyped.converter.cli.Main
 
 import java.io.File
 import java.nio.file.{Files, Paths, StandardCopyOption}
-import scala.collection.immutable.SortedSet
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 
 object ScalablyTypedGenerator extends BleepCodegenScript("ScalablyTypedGenerator") :
 
@@ -45,7 +28,7 @@ object ScalablyTypedGenerator extends BleepCodegenScript("ScalablyTypedGenerator
         val bloopProject: BloopConfig.Project = started.bloopFiles(crossProjectName).forceGet.project
         val scalaVersion =  bloopProject.scala.get.version
 
-        //todo take apart the mill plugin instead
+        //todo maybe take apart the mill plugin instead - or shift away from scalablytyped
         val sctMain: Int = Main.mainNoExit(Array(
           "--directory", projectName.value,
           "--scala", scalaVersion,
