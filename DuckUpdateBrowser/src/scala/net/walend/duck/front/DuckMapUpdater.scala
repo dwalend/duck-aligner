42848eb44d7d0942b6f35bcfe46cb59707d94a5f
dwalend <david@walend.net>
2026-03-15 18:29:22 -0400

    Factored out the map and updater

---
+++ DuckUpdateBrowser/src/scala/net/walend/duck/front/DuckMapUpdater.scala
@@ -0,0 +1,96 @@
+package net.walend.duck.front
+
+import fs2.Stream
+import cats.implicits.*
+import cats.effect.{FiberIO, IO, Resource}
+import net.walend.duckaligner.duckupdates.v0.{DuckId, DuckUpdateService, GeoPoint}
+import org.scalajs.dom.window.window
+import smithy4s.http.UnknownErrorResponse
+
+import scala.concurrent.duration.DurationInt
+
+/**
+ *
+ *
+ * @author David Walend
+ * @since v0.0.0
+ */
+case class DuckMapUpdater(client: DuckUpdateService[IO], eventStore: EventStore[IO], duckId: DuckId):
+
+  private def startUpdates(): Resource[IO, Unit] =
+    val document = window.document//.asInstanceOf[org.scalajs.dom.html.Document] //todo should not need to cast
+    val geoIO: GeoIO = GeoIO(document)
+
+    for
+      _ <- startMap(geoIO, client, eventStore, document)
+      _ <- startPinger(geoIO, client, eventStore, duckId)
+    yield ()
+
+  def updateForever(): IO[Nothing] = startUpdates().useForever
+
+  private def startMap(
+                        geoIO: GeoIO,
+                        client: DuckUpdateService[IO],
+                        eventStore: EventStore[IO],
+                        document: org.scalajs.dom.html.Document,
+                      ): Resource[IO, FiberIO[Unit]] =
+    def mapUpdateStream(duckView: MapLibreDuckView) =
+      Stream.fixedRateStartImmediately[IO](1.seconds, dampen = true)
+        .evalMap(_ => redrawMap(eventStore, duckView))
+        .compile.drain.start
+        .toResource
+
+    for
+      mapLibre <- MapLibreGL.mapLibreResource(geoIO, client)
+      duckView <- MapLibreDuckView.create(mapLibre, document)
+      fiber <- mapUpdateStream(duckView)
+    yield
+      fiber
+
+  private def redrawMap(
+                         eventStore: EventStore[IO],
+                         duckView: MapLibreDuckView,
+                       ): IO[Unit] =
+    val p: IO[Unit] = for
+      eventsFromServer <- eventStore.allEvents
+      sitRep = SitRep(eventsFromServer)
+      now <- IO.realTime
+      _ <- duckView.updateMapLibre(sitRep, now)
+    yield ()
+    p.recover {
+      case uer: UnknownErrorResponse if uer.code == 504 =>
+        println(s"${uer.getMessage}. Will try redrawMap again.")
+      case x =>
+        x.printStackTrace() //todo remove when you haven't trapped a problem in a while
+    }
+    
+  private def startPinger(
+                           geoIO: GeoIO,
+                           client: DuckUpdateService[IO],
+                           eventStore: EventStore[IO],
+                           duckId: DuckId,
+                         ): Resource[IO, FiberIO[Unit]] =
+    Stream.fixedRateStartImmediately[IO](10.seconds, dampen = true) //todo change to every 30 seconds -  or even variable control with some feedback
+      .evalMap(_ => ping(geoIO, client, eventStore, duckId))
+      .compile.drain.start
+      .toResource
+
+  private def ping(
+                    geoIO: GeoIO,
+                    client: DuckUpdateService[IO],
+                    eventStore: EventStore[IO],
+                    duckId: DuckId,
+                  ): IO[Unit] =
+    val p: IO[Unit] = for
+      position: GeoPoint <- geoIO.position()
+      _ <- IO.println(s"Ping from ${position.latitude},${position.longitude}!")
+      eventsFromServer <- eventStore.sendPositionAndGetUpdates(position, client, duckId)
+      sitRep = SitRep(eventsFromServer)
+      _ <- IO.println(sitRep)
+    yield ()
+    p.recover {
+      case uer: UnknownErrorResponse if uer.code == 504 =>
+        println(s"${uer.getMessage}. Will try ping again.")
+    }
+
+    
--- DuckUpdateBrowser/src/scala/net/walend/duck/front/Main.scala
+++ DuckUpdateBrowser/src/scala/net/walend/duck/front/Main.scala
@@ -1,17 +1,13 @@
 package net.walend.duck.front
 
-import cats.implicits.*
 import calico.IOWebApp
-import cats.effect.{FiberIO, IO, Resource}
+import cats.effect.{IO, Resource}
 import fs2.dom.{HtmlDivElement, HtmlElement}
-import net.walend.duckaligner.duckupdates.v0.{DuckId, DuckUpdateService, GeoPoint}
-import fs2.Stream
+import net.walend.duckaligner.duckupdates.v0.DuckUpdateService
 import org.http4s.Uri
-import smithy4s.http.UnknownErrorResponse
 
 import scala.annotation.unused
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
-import scala.concurrent.duration.DurationInt
 
 @JSExportTopLevel("Main")
 object Main extends IOWebApp:
@@ -20,21 +16,7 @@ object Main extends IOWebApp:
   def altMain(): Unit =
     println("in atlMain()")
     main(Array.empty)
-
-  case class DuckMapUpdater(client:DuckUpdateService[IO],eventStore: EventStore[IO],duckId: DuckId):
-
-    private def startUpdates(): Resource[IO, Unit] =
-      val document = window.document.asInstanceOf[org.scalajs.dom.html.Document] //todo should not need to cast
-      val geoIO: GeoIO = GeoIO(document)
-
-      for
-        _ <- startMap(geoIO, client, eventStore, document)
-        _ <- startPinger(geoIO, client, eventStore, duckId)
-      yield ()
-
-    def updateForever(): IO[Nothing] = startUpdates().useForever
-
-
+  
   def render: Resource[IO, HtmlElement[IO]] =
     //todo try delaying creating the map - do it from a UI widget - todo then add the div("map") here
     for
@@ -49,10 +31,8 @@ object Main extends IOWebApp:
     yield
       println("See ducks!")
       appDiv
-
-
-
-  def callDucks(duckMapUpdater:DuckMapUpdater): Resource[IO, HtmlDivElement[IO]] =
+  
+  private def callDucks(duckMapUpdater:DuckMapUpdater): Resource[IO, HtmlDivElement[IO]] =
     import calico.html.io.{*, given}
     import calico.syntax.*
 
@@ -88,68 +68,3 @@ object Main extends IOWebApp:
   private def duckNameFromUriQuery(document:org.scalajs.dom.html.Document):String =
     val uri = Uri.unsafeFromString(document.documentURI)
     uri.query.pairs.toMap.apply("duckName").get
-
-  private def startPinger(
-                           geoIO: GeoIO,
-                           client: DuckUpdateService[IO],
-                           eventStore: EventStore[IO],
-                           duckId: DuckId,
-                         ): Resource[IO, FiberIO[Unit]] =
-    Stream.fixedRateStartImmediately[IO](10.seconds,dampen = true) //todo change to every 30 seconds -  or even variable control with some feedback
-      .evalMap(_ => ping(geoIO, client, eventStore, duckId))
-      .compile.drain.start
-      .toResource
-
-  private def ping(
-                    geoIO: GeoIO,
-                    client: DuckUpdateService[IO],
-                    eventStore: EventStore[IO],
-                    duckId: DuckId,
-                  ): IO[Unit]  =
-    val p:IO[Unit] = for
-      position: GeoPoint <- geoIO.position()
-      _ <- IO.println(s"Ping from ${position.latitude},${position.longitude}!")
-      eventsFromServer <- eventStore.sendPositionAndGetUpdates(position,client,duckId)
-      sitRep = SitRep(eventsFromServer)
-      _ <- IO.println(sitRep)
-    yield ()
-    p.recover {
-      case uer: UnknownErrorResponse if uer.code == 504 =>
-        println(s"${uer.getMessage}. Will try ping again.")
-    }
-
-  private def startMap(
-                        geoIO: GeoIO,
-                        client: DuckUpdateService[IO],
-                        eventStore: EventStore[IO],
-                        document: org.scalajs.dom.html.Document,
-                      ): Resource[IO, FiberIO[Unit]] =
-    def mapUpdateStream(duckView: MapLibreDuckView) =
-      Stream.fixedRateStartImmediately[IO](1.seconds,dampen = true)
-        .evalMap(_ => redrawMap(eventStore, duckView))
-        .compile.drain.start
-        .toResource
-
-    for
-      mapLibre <- MapLibreGL.mapLibreResource(geoIO, client)
-      duckView <- MapLibreDuckView.create(mapLibre, document)
-      fiber <- mapUpdateStream(duckView)
-    yield
-      fiber
-
-  private def redrawMap(
-                          eventStore: EventStore[IO],
-                          duckView: MapLibreDuckView,
-                        ): IO[Unit] =
-    val p: IO[Unit] = for
-      eventsFromServer <- eventStore.allEvents
-      sitRep = SitRep(eventsFromServer)
-      now <- IO.realTime
-      _ <- duckView.updateMapLibre(sitRep, now)
-    yield ()
-    p.recover {
-      case uer: UnknownErrorResponse if uer.code == 504 =>
-        println(s"${uer.getMessage}. Will try redrawMap again.")
-      case x =>
-        x.printStackTrace() //todo remove when you haven't trapped a problem in a while
-    }
